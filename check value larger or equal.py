c37e8e7c181a30bb50e70d748e40e9ec230a5856
Skorpian4 <114504431+Skorpian4@users.noreply.github.com>
2024-01-18 20:35:10 -0100

    Add files via upload

---
+++ check value larger or equal.py
@@ -0,0 +1,10 @@
+num1 = int (input('Digite o primeiro número  '))
+num2 = int (input('Digite o segunde número  '))
+
+if num1 > num2:
+    print('O numero, %d, é maior'%num1)
+elif num1 == num2:
+    print('Os dois numeros são iguais')
+else:
+    print('O numero, %d, é maior'%num2)
+        
